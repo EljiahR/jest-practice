0128632d1c375a9f888c08dd04aed8f5719718f3
Elijah <kcerhajile@gmail.com>
2023-11-06 19:12:44 -0500

    Setup boilerplate for .js and test.js

---
+++ practice.js
@@ -0,0 +1,32 @@
+function capitalize(){
+
+}
+
+function reverseString(){
+
+}
+
+const calculator = {
+    add(){
+
+    },
+    subtract(){
+
+    },
+    divide(){
+
+    },
+    multiply(){
+
+    }
+}
+
+function caesarCipher(){
+
+}
+
+function analyzeArray(){
+
+}
+
+export { capitalize, reverseString, calculator, caesarCipher, analyzeArray }
